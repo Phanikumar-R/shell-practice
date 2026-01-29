26c59da09028091dfbd3355624248f9f15f84314
phani <vsphanikumar777@gmail.com>
2026-01-29 20:22:01 +0530

    actions

---
+++ Variables.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#! /bin/bash
+
+PERSON1="Suresh"
+PERSON2="Ramesh"
+echo "$PERSON1 :: Hello $PERSON2 How are you"
+echo "$PERSON2 :: Doing Good !Thanks for asking $PERSON1"
+echo "$PERSON1 :: What are you doing these days?"
+echo "$PERSON2 :: I am learning Shell Scripting"
+echo "$PERSON1 :: That's great! Shell Scripting is really useful."
