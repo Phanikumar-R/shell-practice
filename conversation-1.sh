a25ef04f03da9b4dd5ac2b5e2ac45ed4a2cea72f
phani <vsphanikumar777@gmail.com>
2026-01-29 20:11:36 +0530

    New conversation for shell

---
+++ conversation-1.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+echo "Suresh :: Hello Ramesh How are you"
+echo "Ramesh :: Doing Good !Thanks for asking Suresh"
+echo "Suresh :: What are you doing these days?"
+echo "Ramesh :: I am learning Shell Scripting"
+echo "Suresh :: That's great! Shell Scripting is really useful."
