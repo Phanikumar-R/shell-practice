f4fc4b2f065da44626e2abc79d91f8c20ab25512
phani <vsphanikumar777@gmail.com>
2026-01-30 13:27:40 +0530

    Learning of data types

--- datatypes.sh
+++ datatypes.sh
@@ -6,3 +6,12 @@ NUM2=phani;
 SUM=$((NUM1+NUM2));
 
 echo "The sum of $NUM1 and $NUM2 is: $SUM";
+
+
+
+echo "Enter you favorite number: "
+read FAVNUM;
+
+FAVUS=$((FAVNUM+10));
+
+echo "Your lucky number  is : $FAVUS";
