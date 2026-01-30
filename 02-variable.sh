fca10739487547743efb72d7fed393af2ffc618c
phani <vsphanikumar777@gmail.com>
2026-01-30 11:12:46 +0530

    New change to variables

--- 02-variable.sh
+++ 02-variable.sh
@@ -5,6 +5,5 @@ read username
 
 echo "Hello, $username! Welcome to the Shell Scripting world."
 
-echo "Mr.$username, how are you today?"
-echo "I hope you are having a great day, $username!"
-echo "$username, keep up the good work with Shell Scripting!"
+echo "Please enter your password:"
+read -s password
