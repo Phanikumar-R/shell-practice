d00c3be708ffa65dc81ea092992defb86e2847a0
phani <vsphanikumar777@gmail.com>
2026-01-30 11:08:45 +0530

    user input

---
+++ 02-variable.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "Enter your username:"
+read username
+
+echo "Hello, $username! Welcome to the Shell Scripting world."
+
+echo "Mr.$username, how are you today?"
+echo "I hope you are having a great day, $username!"
+echo "$username, keep up the good work with Shell Scripting!"
