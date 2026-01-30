51dd1548ca9293649fe3ff847bef560ce70fe3cc
phani <vsphanikumar777@gmail.com>
2026-01-30 20:22:17 +0530

    functions pr

---
+++ functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "Run this script as root user."
+    exit 1
+fi
+
+VALIDATE () {
+    if [ $1 -ne 0 ]; then
+        echo "$2 installation failed."
+        exit 1
+    else
+        echo "$2 installed successfully."    
+    fi
+} 
+
+dnf install nginx -y
+VALIDATE $? "nginx installed"
+dnf install mysql -y
+VALIDATE $? "mysql installed"
+dnf install nodejs -y
+VALIDATE $? "nodejs installed"
